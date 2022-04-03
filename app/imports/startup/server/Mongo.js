c0759bc9937a3b1ab6db4807b94ac6f46d1f1f10
Jeraldine Milla <jeraldinemilla@gmail.com>
2022-04-03 12:27:22 -1000

    implemented digits to manoa-eats issue 1 branch

---
+++ app/imports/startup/server/Mongo.js
@@ -0,0 +1,17 @@
+import { Meteor } from 'meteor/meteor';
+import { Contacts } from '../../api/contact/Contacts';
+/* eslint-disable no-console */
+
+// Initialize the database with a default data document.
+function addContact(data) {
+  console.log(`  Adding: ${data.lastName} (${data.owner})`);
+  Contacts.collection.insert(data);
+}
+
+// Initialize the StuffsCollection if empty.
+if (Contacts.collection.find().count() === 0) {
+  if (Meteor.settings.defaultContacts) {
+    console.log('Creating default Contacts.');
+    Meteor.settings.defaultContacts.map(data => addContact(data));
+  }
+}
