c005ce5cd7506ab0945dc3ec4ce1f6a9931fe1ac
Tyler Baguio <tylerb8@hawaii.edu>
2022-04-14 11:38:50 -1000

    modal added (Not finished)

--- app/imports/ui/components/Restaurant.jsx
+++ app/imports/ui/components/Restaurant.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import { Card, Image, Feed } from 'semantic-ui-react';
+import { Card, Image, Button, Icon, Modal, Header, Input} from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { withRouter, Link } from 'react-router-dom';
-import AddNote from './AddNote';
-import Note from './Note';
+import { withRouter } from 'react-router-dom';
+
+class ModalContainer extends React.Component {
+  componentDidMount() {
+    // eslint-disable-next-line react/prop-types
+    const { startOpen } = this.props;
+
+    if (startOpen) {
+      this.handleOpen();
+    }
+  }
+
+  handleOpen = () => this.setState({ modalOpen: true });
+
+  handleClose = () => this.setState({ modalOpen: false });
+}
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class Restaurant extends React.Component {
+
   render() {
     return (
       <Card>
@@ -24,15 +38,12 @@ class Restaurant extends React.Component {
           </Card.Description>
         </Card.Content>
         <Card.Content extra>
-          <Link to={`/Review/${this.props.restaurant._id}`}>Review</Link>
-        </Card.Content>
-        <Card.Content extra>
-          <Feed>
-            {this.props.notes.map((note, index) => <Note key={index} note={note}/>)}
-          </Feed>
-        </Card.Content>
-        <Card.Content extra>
-          <AddNote owner={this.props.restaurant.owner} restaurantId={this.props.restaurant._id}/>
+          <Modal
+            trigger={<Button primary>Add Review!</Button>}
+            header='Add Review!'
+            content='Call Benjamin regarding the reports.'
+            actions={['cancel', { key: 'done', content: 'Done', positive: true }]}
+          />
         </Card.Content>
       </Card>
     );
