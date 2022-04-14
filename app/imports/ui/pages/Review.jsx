dcad3929e18a578d9d735aad357aa8a1304c993b
Jeraldine Milla <jeraldinemilla@gmail.com>
2022-04-14 11:13:52 -1000

    Review page will be reformated as a modal
    *Do not push change to main*

--- app/imports/ui/pages/Review.jsx
+++ app/imports/ui/pages/Review.jsx
@@ -1,13 +1,59 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
+import { Grid, Segment, Header } from 'semantic-ui-react';
+import { AutoForm, ErrorsField, LongTextField, SubmitField, TextField } from 'uniforms-semantic';
+import swal from 'sweetalert';
+import { Meteor } from 'meteor/meteor';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import SimpleSchema from 'simpl-schema';
+import { Contacts } from '../../api/contact/Contacts';
 
-/** Render a Not Found page if the user enters a URL that doesn't match any route. */
+// Create a schema to specify the structure of the data to appear in the form.
+const formSchema = new SimpleSchema({
+  name: String,
+  email: String,
+  image: String,
+  description: String,
+});
+
+const bridge = new SimpleSchema2Bridge(formSchema);
+
+/** Renders the Page for adding a document. */
 class Review extends React.Component {
+
+  // On submit, insert the data.
+  submit(data, formRef) {
+    const { name, email, image, description } = data;
+    const owner = Meteor.user().username;
+    Contacts.collection.insert({ name, email, image, description, owner },
+      (error) => {
+        if (error) {
+          swal('Error', error.message, 'error');
+        } else {
+          swal('Success', 'Item added successfully', 'success');
+          formRef.reset();
+        }
+      });
+  }
+
+  // Render the form. Use Uniforms: https://github.com/vazco/uniforms
   render() {
+    let fRef = null;
     return (
-      <Header as="h2" textAlign="center">
-        <p>Page not found</p>
-      </Header>
+      <Grid container centered>
+        <Grid.Column>
+          <Header as="h2" textAlign="center" inverted>Write a Review</Header>
+          <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => this.submit(data, fRef)} >
+            <Segment>
+              <TextField name='name'/>
+              <TextField name='email'/>
+              <TextField name='image'/>
+              <LongTextField name='description'/>
+              <SubmitField value='Submit'/>
+              <ErrorsField/>
+            </Segment>
+          </AutoForm>
+        </Grid.Column>
+      </Grid>
     );
   }
 }
