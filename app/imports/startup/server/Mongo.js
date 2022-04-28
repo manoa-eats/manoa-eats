c563e931441c0155516a0d6dd28f34faf4774709
Gavin Peng <blackbullet54321@gmail.com>
2022-04-27 19:01:07 -1000

    restaurant card created when vendor creates their profile

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,6 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import { Contacts } from '../../api/contact/Contacts';
 import { Restaurants } from '../../api/Restaurant/Restaurants';
+import { VendorProfile } from '../../api/vendorprofile/VendorProfile';
+
 /* eslint-disable no-console */
 
 // Initialize the database with a default data document.
@@ -11,7 +13,7 @@ function addContact(data) {
 
 function addRestaurant(data) {
   console.log(`  Adding: ${data.name} (${data.owner})`);
-  Restaurants.collection.insert(data);
+  VendorProfile.insert(data);
 }
 
 // Initialize the StuffsCollection if empty.
@@ -22,7 +24,7 @@ if (Contacts.collection.find().count() === 0) {
   }
 }
 
-if (Restaurants.collection.find().count() === 0) {
+if (VendorProfile.find().count() === 0) {
   if (Meteor.settings.defaultRestaurants) {
     console.log('Creating default Restaurants.');
     Meteor.settings.defaultRestaurants.map(data => addRestaurant(data));
