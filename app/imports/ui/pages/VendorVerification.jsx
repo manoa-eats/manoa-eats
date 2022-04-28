070bc1257670b2fad560f07fdf7ae57be7ec5067
Gavin Peng <blackbullet54321@gmail.com>
2022-04-27 23:50:28 -1000

    Fix vendor verification

--- app/imports/ui/pages/VendorVerification.jsx
+++ app/imports/ui/pages/VendorVerification.jsx
@@ -4,7 +4,7 @@ import { Container, Header, Loader, Card } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import RestaurantAdmin from '../components/RestaurantAdmin';
-import { Restaurants } from '../../api/Restaurant/Restaurants';
+import { VendorProfile } from '../../api/vendorprofile/VendorProfile';
 
 /** Renders a table containing all of the Stuff documents. */
 class VendorVerification extends React.Component {
@@ -36,11 +36,11 @@ VendorVerification.propTypes = {
 // withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
 export default withTracker(() => {
   // Get access to Stuff documents.
-  const subscription = Meteor.subscribe(Restaurants.adminPublicationName);
+  const subscription = Meteor.subscribe('VendorProfile');
   // Determine if the subscription is ready
   const ready = subscription.ready();
   // Get the Stuff documents
-  const restaurants = Restaurants.collection.find({}).fetch();
+  const restaurants = VendorProfile.find({}).fetch();
   return {
     restaurants,
     ready,
