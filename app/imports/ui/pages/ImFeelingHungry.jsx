b76a22489c99ecb850b73c4f1e14a088f0655b08
kristihchinen <chinen.kristi@gmail.com>
2022-04-28 10:01:11 -1000

    Random vendor

--- app/imports/ui/pages/ImFeelingHungry.jsx
+++ app/imports/ui/pages/ImFeelingHungry.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Header, Loader, Card } from 'semantic-ui-react';
+import { Container, Header, Loader, Card, Segment, Button } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
-import swal from 'sweetalert';
 import { Restaurants } from '../../api/Restaurant/Restaurants';
+import { VendorProfile } from '../../api/vendorprofile/VendorProfile';
 import Restaurant from '../components/Restaurant';
 import contact from '../components/Contact';
 import { Reviews } from '../../api/review/Reviews';
@@ -12,42 +12,40 @@ import { Reviews } from '../../api/review/Reviews';
 /** Renders a table containing all of the Stuff documents. */
 class ImFeelingHungry extends React.Component {
 
+  // sets sort / filter method
+  constructor(props) {
+    super(props);
+    this.state = { sortName: true, sortRating: true };
+  }
+
   // If the subscription(s) have been received, render the page, otherwise show a loading icon.
   render() {
-    return (this.props.ready) ? this.renderPage() : <Loader active>I am Feeling Hungry</Loader>;
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
   }
 
   // Render the page once subscriptions have been received.
-  // On click button will pick a random restaurant
   renderPage() {
+    const restaurants = this.props.restaurants;
     return (
-      <Container>
+      <Container id='im-Feeling-Hungry-page'>
         <Header as="h2" textAlign="center" inverted>I am Feeling Hungry</Header>
+        <Segment>
+          <Button className="fluid ui button"
+            onClick={() => this.setState({
+              sortName: false,
+              sortRating: !this.state.sortRating,
+            })}>
+            Pick Another Restaurant
+          </Button>
+        </Segment>
         <Card centered>
-          {this.props.restaurants.map((restaurant, index) => <Restaurant
+          {restaurants.map((restaurant, index) => <Restaurant
             key={index}
             restaurant={restaurant}
             reviews={this.props.reviews.filter(review => (review.contactId === contact._id))}/>)}
         </Card>
-        <button className="fluid ui button">Pick Another Restaurant</button>
       </Container>
     );
-  }
-
-  // <Button title="Pick Another Restaurant" onPress={this.GenerateRandomNumber} />
-
-  // On submit, insert the data.
-  submit(data, formRef) {
-    const { note, owner, contactId, createdAt } = data;
-    Reviews.collection.insert({ note, owner, contactId, createdAt },
-      (error) => {
-        if (error) {
-          swal('Error', error.message, 'error');
-        } else {
-          swal('Success', 'Item added successfully', 'success');
-          formRef.reset();
-        }
-      });
   }
 }
 
@@ -61,16 +59,20 @@ ImFeelingHungry.propTypes = {
 // withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
 export default withTracker(() => {
   // Get access to Stuff documents.
-  const subscription = Meteor.subscribe(Restaurants.userPublicationName);
+  const subscription = Meteor.subscribe('VendorProfile');
+  const subscriptionAdmin = Meteor.subscribe(Restaurants.adminPublicationName);
   const subscription2 = Meteor.subscribe(Reviews.userPublicationName);
+  const subscription2Admin = Meteor.subscribe(Reviews.adminPublicationName);
   // Determine if the subscription is ready
-  const ready = subscription.ready() && subscription2.ready();
+  const ready = subscription.ready() && subscriptionAdmin.ready();
+  const ready2 = subscription2.ready() && subscription2Admin.ready();
   // Get the Stuff documents
-  const restaurants = Restaurants.collection.find({}).fetch();
+  const restaurants = VendorProfile.find({}).fetch();
   const reviews = Reviews.collection.find({}).fetch();
   return {
     restaurants,
     reviews,
     ready,
+    ready2,
   };
 })(ImFeelingHungry);
