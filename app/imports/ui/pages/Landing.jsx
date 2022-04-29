e90c6a779d0094e0b3984be8529b06468aa683d9
Nenye <nenye@ndili.net>
2022-04-29 11:03:10 -1000

    refactored landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -3,6 +3,7 @@ import { Grid, Header, List, Loader, Search } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
+import { VendorProfile } from '../../api/vendorprofile/VendorProfile';
 import { Restaurants } from '../../api/Restaurant/Restaurants';
 
 /** A simple static component to render some text for the landing page. */
@@ -14,11 +15,14 @@ class Landing extends React.Component {
   }
 
   renderPage() {
-    const now = new Date();
-    const openRestaurants = this.props.restaurants.filter(x => x.start <= now.getHours() && now.getHours() <= x.end).map((restaurant, i) => <List.Item
+    const now = new Date().getHours();
+    const openRestaurants = this.props.vendors.filter(x => x.openHour.getHours() <= now && now <= x.closeHour.getHours()).map((restaurant, i) => <List.Item
       key={i}>
       <Header as={'h3'}>{restaurant.name}</Header>
     </List.Item>);
+    console.log(openRestaurants);
+    console.log(now, this.props.vendors[1].openHour.getHours(), this.props.vendors[1].closeHour.getHours());
+    console.log(this.props.vendors[1]);
     return (
       <div id='landing-page'>
         <Grid stackable columns={3} >
@@ -45,20 +49,20 @@ class Landing extends React.Component {
 
 // Require an array of Stuff documents in the props.
 Landing.propTypes = {
-  restaurants: PropTypes.array.isRequired,
+  vendors: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
 export default withTracker(() => {
   // Get access to Stuff documents.
-  const subscription = Meteor.subscribe(Restaurants.userPublicationName);
+  const subscription = Meteor.subscribe('VendorProfile');
   const subscriptionAdmin = Meteor.subscribe(Restaurants.adminPublicationName);
   // Determine if the subscription is ready
   const ready = subscription.ready() && subscriptionAdmin.ready();
   // Get the Stuff documents
-  const restaurants = Restaurants.collection.find({}).fetch();
+  const vendors = VendorProfile.find({}).fetch();
   return {
-    restaurants,
+    vendors,
     ready,
   };
 })(Landing);
